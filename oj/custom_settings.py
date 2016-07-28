3dfb1438f4dfdbd8ecd94ce17480d7496562c04a
peng <ppp8300885@gmail.com>
2016-07-28 22:16:46 +0800

    d

---
+++ oj/custom_settings.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+import os
+
+
+WEBSITE_INFO = {"website_name": u"UCAS OnlineJudge",
+                "website_name_shortcut": u"OnlineJude",
+                "website_footer": u"中国科学院大学计算机与控制学院 <a href=\"http://www.miibeian.gov.cn/\">京ICP备10086号-1</a>",
+                # url结尾没有/
+                "url": u"http://oj.ucasidea.com"}
+
+
+SMTP_CONFIG = {"smtp_server": "smtp.domain.com",
+               "email": "noreply@domain.com",
+               "password": "your_password",
+               "tls": False}
+
+
+# 是否显示所有人的提交, False就只显示自己的
+SHOW_ALL_SUBMISSIONS_LIST = True
+
+
+# please set your own SECRET_KEY to a long random string
+SECRET_KEY = "safgsufygsuygfyeafgsfw63546723r6732bgjhdwveyt2t376"
+
+
